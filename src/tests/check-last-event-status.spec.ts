b4374f40026e42edcec2b5ec308a8c88a7625e4f
Maick Souza <maick_a_s@msn.com>
2021-12-12 13:18:44 -0300

    test(server):rename usecase to SUT

--- src/tests/check-last-event-status.spec.ts
+++ src/tests/check-last-event-status.spec.ts
@@ -23,11 +23,11 @@ class CheckLastEventStatus {
 describe("CheckLastEventStatus", () => {
   it("should get last event data", async () => {
     const loadLastEventRepositoryMock = new LoadLastEventRepositoryMock();
-    const checkLastEventStatus = new CheckLastEventStatus(
+    const systemUnderTest = new CheckLastEventStatus(
       loadLastEventRepositoryMock
     );
 
-    await checkLastEventStatus.execute("any_group_id");
+    await systemUnderTest.execute("any_group_id");
 
     expect(loadLastEventRepositoryMock.groupId).toBe("any_group_id");
     expect(loadLastEventRepositoryMock.callsCount).toBe(1);
