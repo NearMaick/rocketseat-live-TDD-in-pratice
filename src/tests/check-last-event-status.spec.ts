5c3538022f91c210c66a02fc6f19c953d3a9c684
Maick Souza <maick_a_s@msn.com>
2021-12-12 13:33:40 -0300

    test(server):ensure CheckEventStatus returns done when group has no event

--- src/tests/check-last-event-status.spec.ts
+++ src/tests/check-last-event-status.spec.ts
@@ -1,9 +1,11 @@
-class LoadLastEventRepositoryMock implements ILoadLastEventRepository {
+class LoadLastEventRepositorySpy implements ILoadLastEventRepository {
   groupId?: string;
   callsCount = 0;
+  output: undefined;
   async loadLastEvent(groupId: string): Promise<void> {
     this.groupId = groupId;
     this.callsCount += 1;
+    return this.output;
   }
 }
 
@@ -15,14 +17,15 @@ class CheckLastEventStatus {
   constructor(
     private readonly loadLastEventRepository: ILoadLastEventRepository
   ) {}
-  async execute(groupId: string) {
+  async execute(groupId: string): Promise<string> {
     await this.loadLastEventRepository.loadLastEvent(groupId);
+    return "done";
   }
 }
 
 describe("CheckLastEventStatus", () => {
   it("should get last event data", async () => {
-    const loadLastEventRepositoryMock = new LoadLastEventRepositoryMock();
+    const loadLastEventRepositoryMock = new LoadLastEventRepositorySpy();
     const systemUnderTest = new CheckLastEventStatus(
       loadLastEventRepositoryMock
     );
@@ -31,5 +34,17 @@ describe("CheckLastEventStatus", () => {
 
     expect(loadLastEventRepositoryMock.groupId).toBe("any_group_id");
     expect(loadLastEventRepositoryMock.callsCount).toBe(1);
+  });
+
+  it("should return ststus done when group has no event", async () => {
+    const loadLastEventRepositoryMock = new LoadLastEventRepositorySpy();
+    loadLastEventRepositoryMock.output = undefined;
+    const systemUnderTest = new CheckLastEventStatus(
+      loadLastEventRepositoryMock
+    );
+
+    const status = await systemUnderTest.execute("any_group_id");
+
+    expect(status).toBe("done");
   });
 });
